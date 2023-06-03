6ad80912eddbc0592a98312e738952079de055fa
Akshay4955 <akshayshedge11@gmail.com>
2023-06-03 11:46:27 +0530

    Added: Code for add method

---
+++ HashTable/CustomHashTable.cs
@@ -0,0 +1,29 @@
+﻿namespace HashTable;
+
+public class CustomHashTable<T>
+{
+    private class Node
+    {
+        public T Data;
+        public Node Next;
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+    }
+
+    private Node[] Names;
+
+    public CustomHashTable(int size)
+    {
+        Names = new Node[size];
+    }
+
+    public void Add(T data) 
+    {
+        Node newNode = new Node(data);
+        int arrayIndex = Math.Abs(data.GetHashCode()) % Names.Length;
+        Names[arrayIndex] = newNode;
+    }
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace HashTable
+﻿namespace HashTable;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Hash Table...!!!");
-        }
+        Console.WriteLine("Welcome to Hash Table...!!!");
+        CustomHashTable<string> myHashTable = new CustomHashTable<string>(10);
+        myHashTable.Add("Akshay");
+        myHashTable.Add("Rupesh");
+        myHashTable.Add("Rahul");
+        myHashTable.Add("Mangesh");
+        myHashTable.Add("Vishal");
     }
 }
